43e903dfe5be3c0466e7e6b2fc2d4ea1d5cc88bf
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-05-28 17:26:51 +0700

    Portofolio With Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],\
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        Hijau: "#0d9488",
+        Dark: "#0f172a",
+        Pucat: "#64748b",
+      },
+    },
+  },
+  plugins: [],
+};
